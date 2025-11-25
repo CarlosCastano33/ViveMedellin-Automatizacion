0cfe80dfa268d0ff73b16893d7610525359301f6
Carlos Castaño <carlose.castano@udea.edu.co>
2025-11-25 13:37:16 -0500

    Tasks Iniciar con credenciales válidas e inválidas

--- src/main/java/co/edu/udea/vivemedellin/screen/interactions/AccederCon.java
+++ src/main/java/co/edu/udea/vivemedellin/screen/interactions/AccederCon.java
@@ -9,15 +9,16 @@ import net.serenitybdd.screenplay.actions.Click;
 import static co.edu.udea.vivemedellin.screen.userinterfaces.Home.INICIAR_SESION_BUTTON;
 
 public class Clickear implements Interaction {
-    // TODO DEBE ABIR INICIO, LLENAR FORMULARIO Y DARLE ENTER
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+        // TODO DEBE ABIR INICIO, LLENAR FORMULARIO Y DARLE ENTER
         actor.attemptsTo(Click.on(INICIAR_SESION_BUTTON));
+        actor.attemptsTo();
         WaitTime.putWaitTimeOf(1000);
     }
 
-    public static Clickear inicioDeSesion(){
-        return Tasks.instrumented(Clickear.class);
+    public static Clickear inicioDeSesion(String correo, String contrasena){
+        return Tasks.instrumented(Clickear.class, correo, contrasena);
     }
 }
